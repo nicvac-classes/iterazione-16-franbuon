120a474efcbe64fa47e508502d91b711d9cd44c1
franbuon <francescogiuseppe.buono@marconibari.edu.it>
2024-10-24 07:46:17 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,23 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float N, num, max;
+    int i;
+    cout<<"Quanti sono i numeri?"<<endl;
+    cin>>N;
+    i=1;
+    max=0;
+    while(i<=N)
+    {
+        cout<<"Inserisci un numero"<<endl;
+        cin>>num;
+        if(num>max)
+        {
+            max=num;
+        }
+        i=i+1;
+    }
+    cout<<"Il numero massimo tra i numeri è "<<max;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
